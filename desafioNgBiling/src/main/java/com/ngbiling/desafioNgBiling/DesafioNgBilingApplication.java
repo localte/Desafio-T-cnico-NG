4e6148084f424fa6a7bbdeb4b53d958d7575d54f
Breno <brenodf110@gmail.com>
2025-06-21 12:53:34 -0300

    Aplicação base

---
+++ desafioNgBiling/src/main/java/com/ngbiling/desafioNgBiling/DesafioNgBilingApplication.java
@@ -0,0 +1,13 @@
+package com.ngbiling.desafioNgBiling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNgBilingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioNgBilingApplication.class, args);
+	}
+
+}
---
+++ desafioNgBiling/src/test/java/com/ngbiling/desafioNgBiling/DesafioNgBilingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngbiling.desafioNgBiling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioNgBilingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
